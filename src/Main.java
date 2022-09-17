7132f47c4d913a296b6e751795488bce24abe438
Andreeva Nina <nandreewaa@gmail.com>
2022-09-17 22:28:06 +0300

    Add SalesManager and README

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[]arr = {1, 20, 50, 70};
+        SalesManager salesManager = new SalesManager(arr);
+        int max = salesManager.max();
+
+        System.out.println(max);
     }
 }
